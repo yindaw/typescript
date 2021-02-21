7c4af854cd0b198818ebd30acde2081a557be252
jieGit <2857705371@qq.com>
2021-02-21 13:52:47 +0800

    7

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,9 @@
-let say:string = "hello";
-console.log("sasada");
+// let name:string;
+// name = "333";
+
+function add(a: number, b: number) {
+  return a + b;
+}
+let num = add(3, 4);
+let name = "kevin";
+let phone = "13348645645";
