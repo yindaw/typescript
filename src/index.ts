b6b0719703aea0bccd27171a73ca14bc262613c1
jieGit <2857705371@qq.com>
2021-02-21 16:03:33 +0800

    12

--- src/index.ts
+++ src/index.ts
@@ -1,118 +1,55 @@
-// function isOdd(n:number) {
-//   return n % 2 === 0;
-// }s
-
-// let nums: number[];
-// nums = [1, 5, 6, 8];
-
-// let nums: Array<number> = [4, 9, 5];
-
-// function printValues(obj:object) {
-//   const vals = Object.values(obj)
-//   vals.forEach(v => console.log(v))
-// }
-
-// printValues({
-//   name: "asa",
-//   age: 33
-// })
-
-
-// let n:string = undefined;
-// n.toUpperCase();
-
-
-// let name:string | undefined;
-
-
-// function printMenu():void {
-//   console.log("1. 登录");
-//   console.log("2. 注册");
-// }
-
-// function throwError(msg: string): never {
-//   throw new Error(msg);
-// }
-
-
-// function alwaysDoSomething():never {
-//   while (true) {
-//     //...
-//   }
-// }
-
-// let gender: "男" | "女";
-// gender = "女";
-// gender = "男";
-
-
-
-//let arr:[];//arr永远只能取值为一个空数组
-
-
-// let user: {
-//   name: string,
-//   age: number
-// }
-
-// user = {
-//   name: "sda",
-//   age: 65
-// }
-
-
-
-// let tu:[string, number];
-// tu = ["3", 4];
-
-
-
-// let data:any = "sada";
-// let num:number = data;
-
-
-
-
-// type Gender = "男" | "女"
-// type User = {
-//   name:string,
-//   age:number,
-//   gender: Gender
-// }
-
-// let u: User
-
-// u = {
-//   name: "asd",
-//   gender: "男",
-//   age: 45
-// }
-
-// function getUsers(g: Gender):User[] {
-//   return [];
-// }
-
-
-// function combine(a:number, b:number):number;
-// function combine(a:string, b:string):string;
-// function combine(a:number | string, b:number | string): number | string {
-//   if (typeof a === "number" && typeof b === "number") {
-//     return a * b;
-//   } else if (typeof a === "string" && typeof b === "string") {
-//     return a + b;
-//   }
-//   throw new Error("a和b必须是相同的类型");
-// }
-// const result = combine(3, 3);
-
-
-
-function sum(a: number, b: number, c: number = 0) {
- return a = b + c;
+type Deck = NormalCard[]
+type Color =  "♥" | "♠" | "♦" | "♣"
+type NormalCard = {
+  color: Color,
+  mark: number
 }
-sum(3, 4);
-sum(3, 4, 6);
+function createDeck():Deck {
+  const deck: Deck = [];
+  for (let i = 1; i <= 13; i++) {
+    deck.push({
+      mark: i,
+      color: "♠"
+    });
+    deck.push({
+      mark: i,
+      color: "♣"
+    });
+    deck.push({
+      mark: i,
+      color: "♥"
+    });
+    deck.push({
+      mark: i,
+      color: "♦"
+    });
+  }
+  return deck;
+}
 
+function printDeck(deck: Deck) {
+  let result = "\n";
+  deck.forEach((card, i) => {
+    let str = card.color;
+    if (card.mark <= 10) {
+      str += card.mark;
+    } else if (card.mark === 11) {
+      str += "J";
+    } else if (card.mark === 12) {
+      str += "Q";
+    } else {
+      str += "K";
+    }
+    result += str + "\t";
+    if ((i + 1) % 6 ===0) {
+      result += "\n";
+    }
+  })
+  console.log(result);
+}
+
+const deck = createDeck();
+printDeck(deck);
 
 
 
