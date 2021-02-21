4c153f7b2370847d339c0a430f4e4b53f221e020
jieGit <2857705371@qq.com>
2021-02-21 12:54:04 +0800

    3

---
+++ index.ts
@@ -0,0 +1,14 @@
+function getUsername():string | number {
+  if (Math.random() < 0.5) {
+    return "yuan jin";
+  }
+  return 404;
+}
+
+let myname = getUsername();
+if(typeof myname === "string") {
+  myname = myname.split(" ")
+  .filter(it => it)
+  .map(it => it[0].toUpperCase() + it.substr(1))
+  .join(" ");
+}
