dd34900922ad8bf3d194f9bc61bc06ce4b307662
jieGit <2857705371@qq.com>
2021-02-21 20:47:00 +0800

    17

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,3 @@
-enum Permission {
-  Read = 1,
-  Write = 2,
-  Create = 4,
-  Delete = 8
-}
-//使用或运算
-let p:Permission = Permission.Read | Permission.Write;
-//判断是否拥有某个权限
-function hasPermission(target: Permission, per: Permission) {
-  return (target & per) === per;
-}
-//判断变量p是否拥有可读权限
-console.log(hasPermission(p, Permission.Create))
-
-//删除某个权限
-p = p ^ Permission.Write;
+import {sum, name} from "./myModule"
+console.log(name);
+console.log(sum(3, 4));
---
+++ src/myModule.ts
@@ -0,0 +1,10 @@
+export const name = "kevin";
+export function sum(a: number, b: number) {
+  return a + b;
+}
+// export default {
+//   name: "kevin",
+//   sum(a: number, b: number) {
+//     return a + b;
+//   }
+// }
