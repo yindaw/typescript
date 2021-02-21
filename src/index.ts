2c1c38a55a1318339c384b9e7986acfe1f64d5b6
jieGit <2857705371@qq.com>
2021-02-21 14:09:09 +0800

    8

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,22 @@
-// let name:string;
-// name = "333";
+// function isOdd(n:number) {
+//   return n % 2 === 0;
+// }s
 
-function add(a: number, b: number) {
-  return a + b;
-}
-let num = add(3, 4);
-let name = "kevin";
-let phone = "13348645645";
+// let nums: number[];
+// nums = [1, 5, 6, 8];
+
+// let nums: Array<number> = [4, 9, 5];
+
+// function printValues(obj:object) {
+//   const vals = Object.values(obj)
+//   vals.forEach(v => console.log(v))
+// }
+
+// printValues({
+//   name: "asa",
+//   age: 33
+// })
+
+
+let n:string = undefined;
+
