44a623b53ae4afc59ff0edcf75f6170ad3842d28
jieGit <2857705371@qq.com>
2021-02-21 15:00:10 +0800

    9

--- src/index.ts
+++ src/index.ts
@@ -18,5 +18,64 @@
 // })
 
 
-let n:string = undefined;
+// let n:string = undefined;
+// n.toUpperCase();
+
+
+// let name:string | undefined;
+
+
+// function printMenu():void {
+//   console.log("1. 登录");
+//   console.log("2. 注册");
+// }
+
+// function throwError(msg: string): never {
+//   throw new Error(msg);
+// }
+
+
+// function alwaysDoSomething():never {
+//   while (true) {
+//     //...
+//   }
+// }
+
+// let gender: "男" | "女";
+// gender = "女";
+// gender = "男";
+
+
+
+//let arr:[];//arr永远只能取值为一个空数组
+
+
+// let user: {
+//   name: string,
+//   age: number
+// }
+
+// user = {
+//   name: "sda",
+//   age: 65
+// }
+
+
+
+// let tu:[string, number];
+// tu = ["3", 4];
+
+
+
+let data:any = "sada";
+let num:number = data;
+
+
+
+
+
+
+
+
+
 
