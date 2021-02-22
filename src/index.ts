0fe11be0c00aa0e6cd06c0cb1fdc29043a3492e5
jieGit <2857705371@qq.com>
2021-02-22 13:53:51 +0800

    26

--- src/index.ts
+++ src/index.ts
@@ -108,3 +108,21 @@ let u: User = {
 // const arr: ReadonlyArray<number> = [1,5,6];
 
 
+
+
+// interface Duck {
+//   sound: "嘎嘎嘎",
+//   swin(): void
+// }
+
+// let person = {
+//   name: "伪装成鸭子的人",
+//   age: 11,
+//   sound: "嘎嘎嘎" as "嘎嘎嘎",
+//   swin() {
+//     console.log(this.name + "正在游泳，并发出了" + this.souund + "的声音")
+//   }
+// }
+
+
+// let duck: Duck = person
