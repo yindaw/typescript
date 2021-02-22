6c595e631bc9963d3ccdf0c2cbe7f203a3137f51
jieGit <2857705371@qq.com>
2021-02-22 12:30:30 +0800

    24

---
+++ src/index.ts
@@ -0,0 +1,83 @@
+
+
+// interface User {
+//   name: string,
+//   age: number,
+//   sayHello:() => void
+// }
+
+// type User {
+//   name: string,
+//   age: number
+// }
+
+// let u:User = {
+//   name: "sdfg",
+//   age: 33,
+//   sayHello() {
+//     console.log("asddsad")
+//   }
+// }
+
+
+
+// type condition = (n:number) => boolean
+// interface Condition {
+//   (n: number): boolean
+// }
+
+// function sum(numbers:number[], callBack: Condition) {
+//   let s = 0;
+//   numbers.forEach(n => {
+//     if (callBack(n)) {
+//       s += n;
+//     }
+//   })
+//   return s;
+// }
+// const result = sum([3, 4, 8, 2], n => n % 2 !== 0);
+// console.log(result);
+
+
+
+// interface A {
+//   T1: string
+// }
+
+// interface B {
+//   T2: number
+// }
+
+// interface C extends A, B {
+//   T3: boolean
+// }
+
+// let u:C = {
+//   T2: 33,
+//   T1: "sdsdsdf",
+//   T3: true
+// }
+
+
+
+
+
+// type A = {
+//   T1:string
+// }
+
+// type B = {
+//   T2: number
+// }
+
+// type C = {
+//   T1: number
+//   T3: boolean
+// } & A & B
+
+
+// let u:C = {
+//   T2: 33,
+//   T1: "sfafa", 
+//   T3: true
+// }
