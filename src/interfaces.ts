9913888d82056f56e33bad1ebedd43cc2d9fe908
jieGit <2857705371@qq.com>
2021-02-24 16:09:36 +0800

    41

---
+++ src/interfaces.ts
@@ -0,0 +1,29 @@
+export interface IFireShow {
+  singleFire(): void;
+  doubleFire(): void;
+}
+
+
+export interface IWisdomShow {
+  suanshuti(): void;
+  dance(): void;
+}
+
+export interface IBalanceShow {
+  dumuqiao(): void;
+  zougangsi(): void; 
+}
+
+export function hasWisdomShow(ani: object): ani is IWisdomShow {
+  if ((ani as IWisdomShow).dance && (ani as IWisdomShow).suanshuti) {
+    return true;
+  }
+  return false;
+}
+
+export function hasFireShow(ani: object): ani is IFireShow {
+  if ((ani as IFireShow).singleFire && (ani as IFireShow).doubleFire) {
+    return true;
+  }
+  return false;
+}
