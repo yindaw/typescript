7d6162135c4ad17abfd6f364742a1b4850453408
jieGit <2857705371@qq.com>
2021-02-25 22:31:30 +0800

    44

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,26 @@
-//  interface IUser {
-//    name: string,
-//    age: number,
-//    sayHello(this: IUser): void
-//  } 
+class User {
+  @require
+  @range(3, 5)
+  @description("账号")
+  loginid: string; //必须是3-5个字符
+  loginpwd: string; //必须是6-12个字符
+  age: number; //必须是0-100之间的数字
+  gender: "男" | "女";
 
-// const u: IUser = {
-//   name: "sada",
-//   age: 33,
-//   sayHello() {
-//     console.log(this.name, this.age);
-//   }
-// }
-
-
-
-class User{
-  constructor(
-    public name: string,
-    public age: number
-    ) {}
-
-    sayHello(this: User) {
-      console.log(this, this.name, this.age);
-    }
 }
 
-const u = new User("sda", 23);
-const say = u.sayHello;
-say();
+class Article{
+  title: string; //成都必须是4-20个字符串
+}
+
+/**
+ * 统一的验证函数
+ * @param obj
+ */
+function validate(obj: object) {
+  for (const key in obj) {
+    const val = (obj as any)[key];
+    //缺少该属性的验证规则
+    
+  }
+}
