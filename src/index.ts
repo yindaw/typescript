a4a8eb80ddf97cd3c13149d3092b20d8dacb04ce
jieGit <2857705371@qq.com>
2021-02-25 18:09:57 +0800

    43

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,30 @@
-class B {
-  
+//  interface IUser {
+//    name: string,
+//    age: number,
+//    sayHello(this: IUser): void
+//  } 
+
+// const u: IUser = {
+//   name: "sada",
+//   age: 33,
+//   sayHello() {
+//     console.log(this.name, this.age);
+//   }
+// }
+
+
+
+class User{
+  constructor(
+    public name: string,
+    public age: number
+    ) {}
+
+    sayHello(this: User) {
+      console.log(this, this.name, this.age);
+    }
 }
 
-class A {
-  [prop: number]: B
-  [prop: string]: object
-}
+const u = new User("sda", 23);
+const say = u.sayHello;
+say();
