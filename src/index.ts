3f817d635466a1508b7c549950f708ef21b6147e
jieGit <2857705371@qq.com>
2021-02-26 16:26:05 +0800

    45

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,21 @@
-class User {
-  @require
-  @range(3, 5)
-  @description("账号")
-  loginid: string; //必须是3-5个字符
-  loginpwd: string; //必须是6-12个字符
-  age: number; //必须是0-100之间的数字
-  gender: "男" | "女";
-
-}
-
-class Article{
-  title: string; //成都必须是4-20个字符串
-}
-
-/**
- * 统一的验证函数
- * @param obj
- */
-function validate(obj: object) {
-  for (const key in obj) {
-    const val = (obj as any)[key];
-    //缺少该属性的验证规则
-    
+type constructor = new (...args: any[]) => object;
+function d1() {
+  console.log("d1");
+  return function (target: constructor) {
+    console.log("d1 decorator");
   }
+}
+
+function d2() {
+  console.log("d2");
+  return function (target: constructor) {
+    console.log("d2 decorator");
+  }
+}
+
+@d1()
+@d2()
+
+class A {
+  prop1: string
 }
